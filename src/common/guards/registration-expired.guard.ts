855a76dc1f746761198b7b1c2ab633de68090b08
partnadem <team@forasoft.com>
2021-12-18 13:25:54 +0300

    closed registration

--- src/common/guards/registration-expired.guard.ts
+++ src/common/guards/registration-expired.guard.ts
@@ -7,19 +7,14 @@ export class RegistrationExpiredGuard implements CanActivate {
   private readonly ADMIN_IDS = [];
 
   canActivate(context: ExecutionContext): boolean {
-    const ctx = TelegrafExecutionContext.create(context);
-    const { from } = ctx.getContext<Context>();
-
     const today = new Date();
 
     const day = today.getDate();
     const hours = today.getHours();
 
-    if (day >= 18 && hours >= 12) {
-      throw new TelegrafException(
-        'Упс, регистрация в Тайном Санте уже завершена.',
-      );
-    }
+    throw new TelegrafException(
+      'Упс, регистрация в Тайном Санте уже завершена.',
+    );
 
     return true;
   }
